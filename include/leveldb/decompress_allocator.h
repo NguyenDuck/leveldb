b74ec3152e9c929e81c292e34d491bfaccccda74
Yeabsira Mekonnen <ymekonnen@microsoft.com>
2025-02-06 17:07:11 -0800

    changed decompress_allocator.h spacing to match other leveldb files + reverting comment deletion in options.h

--- include/leveldb/decompress_allocator.h
+++ include/leveldb/decompress_allocator.h
@@ -8,19 +8,17 @@
 #include <string>
 
 namespace leveldb {
-	class DLLX DecompressAllocator {
-	public:
-		virtual ~DecompressAllocator();
+class DLLX DecompressAllocator {
+ public:
+  virtual ~DecompressAllocator();
+  virtual std::string get();
+  virtual void release(std::string&& string);
+  virtual void prune();
 
-		virtual std::string get();
-		virtual void release(std::string&& string);
-
-		virtual void prune();
-
-	protected:
-		std::mutex mutex;
-		std::vector<std::string> stack;
-	};
+ protected:
+  std::mutex mutex;
+  std::vector<std::string> stack;
+};
 }
 
 #endif
